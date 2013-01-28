b554a0b3e86f22d7a765c3f90fbae1909b7fc4b5
Peter Hoffmann <Hoffmann.P@gmx.net>
2013-01-28 09:21:20 +0100

    Vollständig fertig, aber ohne Ping

--- src/wakeUp.go
+++ src/wakeUp.go
@@ -5,17 +5,27 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"networking"
 	"os"
-	"wol"
 )
 
-type mainObject struct {
-	t *template.Template
-	s []struct {
+type Config struct {
+	Port  int
+	Nodes []struct {
 		Name string
 		IP   string
 		MAC  string
 	}
+}
+
+type node struct {
+	IP  string
+	MAC string
+}
+
+type mainObject struct {
+	t *template.Template
+	s map[string]node
 }
 
 func status(ip string) string {
@@ -31,9 +41,14 @@ func main() {
 		fmt.Println("error:", err)
 	}
 	dec := json.NewDecoder(config)
-	err = dec.Decode(&me.s)
+	var cfg Config
+	err = dec.Decode(&cfg)
 	if err != nil {
 		fmt.Println("error:", err)
+	}
+	me.s = make(map[string]node, len(cfg.Nodes))
+	for _, n := range cfg.Nodes {
+		me.s[n.Name] = node{n.IP, n.MAC}
 	}
 
 	// read template
@@ -44,27 +59,43 @@ func main() {
 
 	// start server
 	http.Handle("/", me)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
+}
+
+func (me *mainObject) wol(Name string) {
+	networking.SendMagicPacket(me.s[Name].MAC, me.s[Name].IP)
+	println("Waking Up ( IP=", me.s[Name].IP, ")")
+}
+
+func (me *mainObject) netstat(w http.ResponseWriter, Name string) {
+	status := networking.Netstat(me.s[Name].IP)
+	println("IP", me.s[Name].IP, "online", status)
+	if status {
+		fmt.Fprint(w, "<span class='online'>online</span>")
+	} else {
+		fmt.Fprint(w, "<span class='offline'>offline</span>")
+	}
 }
 
 func (me *mainObject) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/wake_up.php" {
 		r.ParseForm()
-		IP := r.Form.Get("IP")
-		MAC := r.Form.Get("MAC")
-		for _, s := range me.s {
-			if (s.MAC == MAC) && (s.IP == IP) {
-				wol.SendMagicPacket(s.MAC, s.IP)
-				println("Waking Up ( IP=", s.IP, ")")
-			}
-		}
+		me.wol(r.Form.Get("Name"))
 		http.Redirect(w, r, "/index.php", 302)
 		return
 	}
+
+	if r.URL.Path == "/netstat.php" {
+		r.ParseForm()
+		me.netstat(w, r.Form.Get("Name"))
+		return
+	}
+
 	if r.URL.Path != "/index.php" {
 		http.Redirect(w, r, "/index.php", 302)
 		return
 	}
+
 	err := me.t.Execute(w, me.s)
 	if err != nil {
 		fmt.Println("error:", err)
